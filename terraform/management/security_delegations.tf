e2b7b62ba5e8b43bede05ef3ee7dc10c8a3d4d5b
pshoxxx <60489201+pshoxxx@users.noreply.github.com>
2026-05-26 17:38:42 -0700

    Adding in additional service disablement for full cleanup

--- terraform/management/security_delegations.tf
+++ terraform/management/security_delegations.tf
@@ -110,20 +110,12 @@ resource "aws_guardduty_detector" "management" {
       }
     }
   }
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 resource "aws_guardduty_organization_admin_account" "main" {
   admin_account_id = local.security_account_id
 
   depends_on = [aws_guardduty_detector.management]
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 # =====================================================================
@@ -139,20 +131,12 @@ resource "aws_securityhub_account" "management" {
   enable_default_standards  = false
   control_finding_generator = "STANDARD_CONTROL"
   auto_enable_controls      = true
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 resource "aws_securityhub_organization_admin_account" "main" {
   admin_account_id = local.security_account_id
 
   depends_on = [aws_securityhub_account.management]
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 # =====================================================================
